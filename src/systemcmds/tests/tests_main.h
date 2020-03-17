581be8d1a0504a3904687a105b2e2efd42ec671f
Lorenz Meier <lorenz@px4.io>
2020-03-17 12:05:42 +0100

    Add CLI tests for basic soak testing This is in no way a complete test suite but is intended as a starting point to get minimal test coverage for CLI handling tests in. This is necessary to QA the CLI handling (e.g. removal of commands) and to ensure that we are exercising these code paths in stack check and alike tests.

--- src/systemcmds/tests/tests_main.h
+++ src/systemcmds/tests/tests_main.h
@@ -85,6 +85,7 @@ extern int test_uart_console(int argc, char *argv[]);
 extern int test_uart_loopback(int argc, char *argv[]);
 extern int test_uart_send(int argc, char *argv[]);
 extern int test_versioning(int argc, char *argv[]);
+extern int test_cli(int argc, char *argv[]);
 
 /* external */
 extern int commander_tests_main(int argc, char *argv[]);
