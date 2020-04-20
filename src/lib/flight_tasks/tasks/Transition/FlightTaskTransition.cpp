854b158ec7ccb7fc4033f91f9f46ae1f9e92b7d2
bresch <brescianimathieu@gmail.com>
2020-04-20 10:38:50 +0200

    FlightTask: in update function, include returned value of parent class

--- src/lib/flight_tasks/tasks/Transition/FlightTaskTransition.cpp
+++ src/lib/flight_tasks/tasks/Transition/FlightTaskTransition.cpp
@@ -60,7 +60,7 @@ void FlightTaskTransition::checkSetpoints(vehicle_local_position_setpoint_s &set
 
 bool FlightTaskTransition::update()
 {
-	FlightTask::update();
+	bool ret = FlightTask::update();
 	_acceleration_setpoint.xy() = matrix::Vector2f(0.f, 0.f);
 	// demand zero vertical velocity and level attitude
 	// tailsitters will override attitude and thrust setpoint
@@ -72,5 +72,5 @@ bool FlightTaskTransition::update()
 	_position_setpoint(2) = NAN;
 
 	_yaw_setpoint = NAN;
-	return true;
+	return ret;
 }
