0289b132e016dc6b763f6fa11cf30bcf0b776f35
David Jablonski <dayjaby@gmail.com>
2020-05-03 13:56:21 +0200

    added FLIGHT_INFORMATION stream

--- src/modules/mavlink/mavlink_receiver.h
+++ src/modules/mavlink/mavlink_receiver.h
@@ -200,9 +200,6 @@ private:
 
 	bool evaluate_target_ok(int command, int target_system, int target_component);
 
-	void send_flight_information();
-	void send_storage_information(int storage_id);
-
 	void fill_thrust(float *thrust_body_array, uint8_t vehicle_type, float thrust);
 
 	/**
