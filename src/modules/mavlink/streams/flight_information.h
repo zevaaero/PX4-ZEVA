70c0555a56bd4e0b2261c0c279fdd043109af1d4
Martina Rivizzigno <martina@rivizzigno.it>
2020-05-12 10:32:16 +0200

    Support for gimbal v2 protocol
    - add command to request a message
    - add gimbal attitude message
    
    mavlink_receiver handle GIMBAL_MANAGER_SET_ATTITUDE
    
    first implementation of new vmount input MavlinkGimbalV2
    - setup class
    - decode gimbal_manager_set_attitude in ControlData
    
    add gimbal information message
    
    add gimbal manager information and vehicle command ack
    
    mavlink messages: add stream for GIMBAL_MANAGER_INFORMATION
    
    mavlink_receiver: handle GIMBAL_DEVICE_INFORMATION
    
    remove mavlink cmd handling from vmount input MavlinkGimbalV2
    
    complete gimbal manager:
    - send out fake gimbal_device_information for dummy gimbals
    - complete ROI handling with nudging
    
    small fixes
    
    fix typos
    
    cleanup
    - gimbal device information
    - flags lock
    - check sanity of string
    
    add support for CMD_DO_GIMBAL_MANAGER_ATTITUDE
    
    stream GimbalDeviceAttitudeStatus for dummy gimbals
    - add uROB gimbal_attitude_status
    - fill status in vmount output_rc for dummy gimbals not able to send the
    status themselves
    - stream mavlink GimbalDeviceAttitudeStatus
    
    better handle the request for gimbal infomation request
    
    clean up
    
    bring gimbal information back on vmount init
    
    add new gimbal message to mavlink normal stream
    
    fix publication of gimbal device information
    
    rename gimbal_attitude_status to gimbal_device_attitude_status
    
    stream gimbal_manager_status at 5Hz
    
    mavlink: send information only on request
    
    Sending the information message once on request should now work and we
    don't need to keep publishing it.
    
    mavlink: debug output for now
    
    make sure to copy over control data
    
    mavlink: add missing copyright header, pragma once
    
    mavlink: address review comments
    
    mavlink: handle stream not updated
    
    Our answer does not just depend on whether the stream was found but
    whether we actually were able to send out an update.
    
    mavlink: remove outdated comment
    
    vmount: add option for yaw stabilization only
    
    The stabilize flag is used for gimbals which do not have an internal IMU
    and need the autopilot's attitude in order to do stabilization. These
    gimbals are probably quite rare by now but it makes sense to keep the
    functionality given it can e.g. be used by simple servo gimbals for
    sensors other than highres cameras.
    
    The stabilize flag can also be re-used for gimbals which are capable of
    stabilizing pitch and roll but not absolute yaw (e.g. locked to North).
    For such gimbals we can now set the param MNT_DO_STAB to 2.
    
    We still support configuring which axes are stabilized by the
    MAVLink command DO_MOUNT_CONFIGURE, however, this is generally not
    recommended anymore.
    
    vmount: fix incorrect check for bit flag
    
    mavlink_messages: remove debug message
    
    Signed-off-by: Claudio Micheli <claudio@auterion.com>
    
    use device id
    
    remove debug print
    
    gimbal attitude fix mistake
    
    clang tidy fix
    
    split:
    - gimbal_attitude -> gimbal_device_set_attitude, gimbal_manager_set_attitude
    - gimbal_information -> gimbal_device_informatio, gimbal_manager_information
    
    add gimbal protocol messages to rtps msg ids
    
    support set attitude for gimbal directly speaking mavlink
    
    clean up gimbal urob messages
    
    vmount: address a few small review comments
    
    vmount: split output into v1 and v2 protocol
    
    This way we can continue to support the MAVLink v1 protocol. Also, we
    don't send the old vehicle commands when actually using the new v2
    protocol.
    
    vmount: config via ctor instead of duplicate param
    
    vmount: use loop to poll all topics
    
    Otherwise we might give up too soon and miss some data, or run too fast
    based on commands that have nothing to do with the gimbal.
    
    typhoon_h480: use gimbal v2 protocol, use yaw stab
    
    Let's by default use the v2 protocol with typhoon_h480 and enable yaw
    lock mode by stabilizing yaw.

--- src/modules/mavlink/streams/flight_information.h
+++ src/modules/mavlink/streams/flight_information.h
@@ -1,5 +1,37 @@
-#ifndef MAVLINK_STREAM_FLIGHT_INFORMATION_H
-#define MAVLINK_STREAM_FLIGHT_INFORMATION_H
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
 
 #include "../mavlink_messages.h"
 
@@ -39,13 +71,17 @@ public:
 	}
 private:
 	uORB::Subscription _armed_sub{ORB_ID(actuator_armed)};
+	param_t _param_com_flight_uuid;
 
 	/* do not allow top copying this class */
 	MavlinkStreamFlightInformation(MavlinkStreamFlightInformation &) = delete;
 	MavlinkStreamFlightInformation &operator = (const MavlinkStreamFlightInformation &) = delete;
 protected:
 	explicit MavlinkStreamFlightInformation(Mavlink *mavlink) : MavlinkStream(mavlink)
-	{}
+	{
+
+		_param_com_flight_uuid = param_find("COM_FLIGHT_UUID");
+	}
 
 	bool send(const hrt_abstime t) override
 	{
@@ -53,9 +89,8 @@ protected:
 		bool ret = _armed_sub.copy(&actuator_armed);
 
 		if (ret && actuator_armed.timestamp != 0) {
-			const param_t param_com_flight_uuid = param_find("COM_FLIGHT_UUID");
 			int32_t flight_uuid;
-			param_get(param_com_flight_uuid, &flight_uuid);
+			param_get(_param_com_flight_uuid, &flight_uuid);
 
 			mavlink_flight_information_t flight_info{};
 			flight_info.flight_uuid = static_cast<uint64_t>(flight_uuid);
@@ -67,5 +102,3 @@ protected:
 		return ret;
 	}
 };
-
-#endif
--- src/modules/mavlink/streams/storage_information.h
+++ src/modules/mavlink/streams/storage_information.h
@@ -1,5 +1,37 @@
-#ifndef MAVLINK_STREAM_STORAGE_INFORMATION_H
-#define MAVLINK_STREAM_STORAGE_INFORMATION_H
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
 
 #include "../mavlink_messages.h"
 
@@ -48,11 +80,11 @@ public:
 	bool request_message(float param2, float param3, float param4,
 			     float param5, float param6, float param7) override
 	{
-		storage_id = (int)roundf(param2);
+		_storage_id = (int)roundf(param2);
 		return send(hrt_absolute_time());
 	}
 private:
-	int storage_id = 0;
+	int _storage_id = 0;
 
 	/* do not allow top copying this class */
 	MavlinkStreamStorageInformation(MavlinkStreamStorageInformation &) = delete;
@@ -68,7 +100,7 @@ protected:
 		mavlink_storage_information_t storage_info{};
 		const char *microsd_dir = PX4_STORAGEDIR;
 
-		if (storage_id == 0 || storage_id == 1) { // request is for all or the first storage
+		if (_storage_id == 0 || _storage_id == 1) { // request is for all or the first storage
 			storage_info.storage_id = 1;
 
 			struct statfs statfs_buf;
@@ -101,5 +133,3 @@ protected:
 		return true;
 	}
 };
-
-#endif
