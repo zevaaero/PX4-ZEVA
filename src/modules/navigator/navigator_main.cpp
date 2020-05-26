b5c1874a1a8324a0069705c01c7abc265ca02fef
Silvan Fuhrer <silvan@auterion.com>
2020-05-26 11:06:00 +0200

    Navigator: reset disable_wather_vane to false per default
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -893,7 +893,7 @@ Navigator::reset_position_setpoint(position_setpoint_s &sp)
 	sp.cruising_throttle = get_cruising_throttle();
 	sp.valid = false;
 	sp.type = position_setpoint_s::SETPOINT_TYPE_IDLE;
-	sp.disable_weather_vane = true;
+	sp.disable_weather_vane = false;
 }
 
 float
