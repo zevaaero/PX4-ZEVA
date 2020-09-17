b5e0336b45dd1922d0458c2d174a365e04b9740d
Silvan Fuhrer <silvan@auterion.com>
2020-09-17 09:41:23 +0200

    v5x board sensors: disable startup of internal baros if corresponding param is set
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/sensors/sensor_params.c
+++ src/modules/sensors/sensor_params.c
@@ -223,3 +223,15 @@ PARAM_DEFINE_INT32(SENS_IMU_MODE, 1);
  * @group Sensors
  */
 PARAM_DEFINE_INT32(SENS_SKN_ROT_U, 0);
+
+/**
+ * Enable internal barometers
+ *
+ * For systems with an external barometer, this should be set to false to make sure that the external is used.
+ *
+ * @boolean
+ * @reboot_required true
+ * @category system
+ * @group Sensors
+ */
+PARAM_DEFINE_INT32(SENS_INT_BARO_EN, 1);
