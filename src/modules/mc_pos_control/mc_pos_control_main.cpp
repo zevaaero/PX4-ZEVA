d39a4e575e279fa34791f95da1f1df4aed4f1d4e
xdwgood <1308455330@qq.com>
2020-10-30 09:04:23 +0800

    Do not run transition flight task when not enable altitude control

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -735,7 +735,8 @@ MulticopterPositionControl::start_flight_task()
 		_flight_tasks.switchTask(FlightTaskIndex::None);
 	}
 
-	if (_vehicle_status.in_transition_mode) {
+	// Do not run transition flight task if altitude control is not enabled (e.g. in Stabilized or Acro flight mode)
+	if (_vehicle_status.in_transition_mode && _control_mode.flag_control_altitude_enabled) {
 		should_disable_task = false;
 		FlightTaskError error = _flight_tasks.switchTask(FlightTaskIndex::Transition);
 
