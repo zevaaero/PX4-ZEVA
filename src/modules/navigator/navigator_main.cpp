49edace5b6912e855a6eda5db1bd271aeef08c7b
Silvan Fuhrer <silvan@auterion.com>
2020-11-11 17:26:10 +0100

    Navigator: increase stack of the navigator task, and revert increase of it in main
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -861,7 +861,7 @@ int Navigator::task_spawn(int argc, char *argv[])
 	_task_id = px4_task_spawn_cmd("navigator",
 				      SCHED_DEFAULT,
 				      SCHED_PRIORITY_NAVIGATION,
-				      1800,
+				      1900,
 				      (px4_main_t)&run_trampoline,
 				      (char *const *)argv);
 
