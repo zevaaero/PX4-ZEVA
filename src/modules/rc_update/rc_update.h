558485cd3c127b5732f33941583320f40997d63c
Matthias Grob <maetugr@gmail.com>
2020-12-07 14:50:32 +0100

    rc_update: simplify button press mode slot logic

--- src/modules/rc_update/rc_update.h
+++ src/modules/rc_update/rc_update.h
@@ -169,9 +169,8 @@ private:
 
 	hrt_abstime _last_rc_to_param_map_time = 0;
 
-	uint8_t _button_press_slot_to_send{0};
+
 	uint8_t _potential_button_press_slot{0};
-	bool 	_button_press_already_sent{false};
 	systemlib::Hysteresis _button_pressed_hysteresis{false};
 
 	perf_counter_t		_loop_perf;			/**< loop performance counter */
