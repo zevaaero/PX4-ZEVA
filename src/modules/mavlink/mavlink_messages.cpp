8ba49a91e9fafc912a27d14ce251f051bf6a886d
TSC21 <n.marques21@hotmail.com>
2020-12-17 10:42:29 +0000

    modules: mavlink: add stream for COMMAND_CANCEL

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -107,6 +107,7 @@
 #include <uORB/topics/vehicle_attitude.h>
 #include <uORB/topics/vehicle_attitude_setpoint.h>
 #include <uORB/topics/vehicle_command.h>
+#include <uORB/topics/vehicle_command_cancel.h>
 #include <uORB/topics/vehicle_control_mode.h>
 #include <uORB/topics/vehicle_global_position.h>
 #include <uORB/topics/vehicle_gps_position.h>
@@ -531,6 +532,71 @@ protected:
 		MavlinkCommandSender::instance().check_timeout(_mavlink->get_channel());
 
 		return sent;
+	}
+};
+
+class MavlinkStreamCommandCancel : public MavlinkStream
+{
+public:
+	const char *get_name() const override
+	{
+		return MavlinkStreamCommandCancel::get_name_static();
+	}
+
+	static constexpr const char *get_name_static()
+	{
+		return "COMMAND_CANCEL";
+	}
+
+	static constexpr uint16_t get_id_static()
+	{
+		return MAVLINK_MSG_ID_COMMAND_CANCEL;
+	}
+
+	uint16_t get_id() override
+	{
+		return get_id_static();
+	}
+
+	static MavlinkStream *new_instance(Mavlink *mavlink)
+	{
+		return new MavlinkStreamCommandCancel(mavlink);
+	}
+
+	unsigned get_size() override
+	{
+		return MAVLINK_MSG_ID_SYS_STATUS_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
+	}
+
+private:
+	uORB::Subscription _cmd_cancel_sub{ORB_ID(vehicle_command_cancel)};
+
+	/* do not allow top copying this class */
+	MavlinkStreamCommandCancel(MavlinkStreamCommandCancel &) = delete;
+	MavlinkStreamCommandCancel &operator = (const MavlinkStreamCommandCancel &) = delete;
+
+protected:
+	explicit MavlinkStreamCommandCancel(Mavlink *mavlink) : MavlinkStream(mavlink)
+	{}
+
+	bool send(const hrt_abstime t) override
+	{
+		struct vehicle_command_cancel_s cmd_cancel;
+
+		if (_cmd_cancel_sub.update(&cmd_cancel)) {
+
+			mavlink_command_cancel_t msg{};
+
+			msg.command = cmd_cancel.command;
+			msg.target_system = cmd_cancel.target_system;
+			msg.target_component = cmd_cancel.target_component;
+
+			mavlink_msg_command_cancel_send_struct(_mavlink->get_channel(), &msg);
+
+			return true;
+		}
+
+		return false;
 	}
 };
 
@@ -5927,6 +5993,7 @@ static const StreamListItem streams_list[] = {
 	create_stream_list_item<MavlinkStreamHeartbeat>(),
 	create_stream_list_item<MavlinkStreamStatustext>(),
 	create_stream_list_item<MavlinkStreamCommandLong>(),
+	create_stream_list_item<MavlinkStreamCommandCancel>(),
 	create_stream_list_item<MavlinkStreamSysStatus>(),
 	create_stream_list_item<MavlinkStreamBatteryStatus>(),
 	create_stream_list_item<MavlinkStreamSmartBatteryInfo>(),
