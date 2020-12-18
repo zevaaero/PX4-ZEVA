b0532c8b36c12c190cf69d6f6a2a831d74631872
TSC21 <n.marques21@hotmail.com>
2020-12-18 14:40:48 +0000

    mavlink_receiver: change target system id for MAV_CMD_WAYPOINT_USER_1 when it is received

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -581,7 +581,7 @@ void MavlinkReceiver::handle_message_command_both(mavlink_message_t *msg, const 
 
 	} else if (cmd_mavlink.command == MAV_CMD_WAYPOINT_USER_1) {
 		vehicle_command_s custom_action_cmd = vehicle_command;
-		custom_action_cmd.target_system = 1;
+		custom_action_cmd.target_system = 0;
 		custom_action_cmd.target_component = MAV_COMP_ID_PATHPLANNER;
 		custom_action_cmd.from_external = false;
 
