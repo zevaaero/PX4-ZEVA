03146dd7b8a4df79a476fcb3068201673bb4c73d
Silvan Fuhrer <silvan@auterion.com>
2020-12-22 13:46:22 +0100

    Standard VTOL: set idle PWM during backtransition
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/vtol_att_control/standard.cpp
+++ src/modules/vtol_att_control/standard.cpp
@@ -305,7 +305,12 @@ void Standard::update_transition_state()
 
 		}
 
-		set_all_motor_state(motor_state::ENABLED);
+		set_all_motor_state(motor_state::ENABLED); // turn on all motors
+
+		// set idle speed for MC actuators
+		if (!_flag_idle_mc) {
+			_flag_idle_mc = set_idle_mc();
+		}
 	}
 
 	mc_weight = math::constrain(mc_weight, 0.0f, 1.0f);
