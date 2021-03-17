39016b0cbcd0ba1c5d8d2e5f8d22be8d4e403ce7
Claudio Micheli <claudio@auterion.com>
2021-03-17 12:24:27 +0100

    logged_topics: increase battery_status log @ 10Hz
    Signed-off-by: Claudio Micheli <claudio@auterion.com>

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -141,7 +141,7 @@ void LoggedTopics::add_default_topics()
 	add_topic_multi("wind_estimate", 1000); // published by both ekf2 and airspeed_selector
 
 	// log all raw sensors at minimal rate (at least 1 Hz)
-	add_topic_multi("battery_status", 300, 2);
+	add_topic_multi("battery_status", 100, 2);
 	add_topic_multi("differential_pressure", 1000, 2);
 	add_topic_multi("distance_sensor", 1000);
 	add_topic_multi("optical_flow", 1000, 1);
