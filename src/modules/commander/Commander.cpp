208bcd88e24d653383b308c6c4b24a783b0b7db0
Matthias Grob <maetugr@gmail.com>
2021-03-30 20:53:53 +0200

    Commander: switch main_state functions to pass by reference

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -2322,7 +2322,7 @@ Commander::run()
 				}
 
 				// evaluate the main state machine according to mode switches
-				if (set_main_state(&_status_changed) == TRANSITION_CHANGED) {
+				if (set_main_state(_status_changed) == TRANSITION_CHANGED) {
 					// play tune on mode change only if armed, blink LED always
 					tune_positive(_armed.armed);
 					_status_changed = true;
@@ -2915,7 +2915,7 @@ Commander::control_status_leds(bool changed, const uint8_t battery_warning)
 }
 
 transition_result_t
-Commander::set_main_state(bool *changed)
+Commander::set_main_state(bool &changed)
 {
 	if (_safety.override_available && _safety.override_enabled) {
 		return set_main_state_override_on(changed);
@@ -2926,11 +2926,11 @@ Commander::set_main_state(bool *changed)
 }
 
 transition_result_t
-Commander::set_main_state_override_on(bool *changed)
+Commander::set_main_state_override_on(bool &changed)
 {
 	const transition_result_t res = main_state_transition(_status, commander_state_s::MAIN_STATE_MANUAL, _status_flags,
-					&_internal_state);
-	*changed = (res == TRANSITION_CHANGED);
+					_internal_state);
+	changed = (res == TRANSITION_CHANGED);
 
 	return res;
 }
