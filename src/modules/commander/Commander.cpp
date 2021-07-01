005385d128050bcf8b6eec5e37beec278c94f858
Matthias Grob <maetugr@gmail.com>
2021-07-01 19:13:43 +0200

    Commander: call battery_status_check on update instead of early return

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -2054,7 +2054,9 @@ Commander::run()
 
 		_cpuload_sub.update(&_cpuload);
 
-		battery_status_check();
+		if (_battery_status_subs.updated()) {
+			battery_status_check();
+		}
 
 		/* If in INIT state, try to proceed to STANDBY state */
 		if (!_status_flags.condition_calibration_enabled && _status.arming_state == vehicle_status_s::ARMING_STATE_INIT) {
@@ -3735,13 +3737,6 @@ void Commander::avoidance_check()
 
 void Commander::battery_status_check()
 {
-	// We need to update the status flag if ANY battery is updated, because the system source might have
-	// changed, or might be nothing (if there is no battery connected)
-	if (!_battery_status_subs.updated()) {
-		// Nothing has changed since the last time this function was called, so nothing needs to be done now.
-		return;
-	}
-
 	int battery_required_count{0};
 
 	// There are possibly multiple batteries, and we can't know which ones serve which purpose. So the safest
