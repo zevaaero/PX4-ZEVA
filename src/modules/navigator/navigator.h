df3f07d25a6f0297c905ddbe842a39518131416b
Igor Mišić <igy1000mb@gmail.com>
2021-07-06 11:32:46 +0200

    navigator: reduce traffic alert messages

--- src/modules/navigator/navigator.h
+++ src/modules/navigator/navigator.h
@@ -154,6 +154,11 @@ public:
 	void		check_traffic();
 
 	/**
+	 * Buffer for air traffic to control the amount of messages sent to a user
+	 */
+	bool		buffer_air_traffic(uint32_t icao_address);
+
+	/**
 	 * Setters
 	 */
 	void		set_can_loiter_at_sp(bool can_loiter) { _can_loiter_at_sp = can_loiter; }
@@ -382,6 +387,11 @@ private:
 		(ParamFloat<px4::params::NAV_GPSF_LT>) _param_nav_gpsf_lt	/**< GPS failure fixed-bank loitering time*/
 	)
 
+	struct traffic_buffer_s {
+		uint32_t 	icao_address;
+		hrt_abstime timestamp;
+	};
+
 	int		_local_pos_sub{-1};
 	int		_mission_sub{-1};
 	int		_vehicle_status_sub{-1};
@@ -484,6 +494,8 @@ private:
 
 	bool _mission_landing_in_progress{false};	// this flag gets set if the mission is currently executing on a landing pattern
 	// if mission mode is inactive, this flag will be cleared after 2 seconds
+
+	traffic_buffer_s _traffic_buffer;
 
 	// update subscriptions
 	void		params_update();
