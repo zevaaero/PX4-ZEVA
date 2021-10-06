f840acfc2804031b867f78d55a2d86c683b3fe81
Igor Mišić <igy1000mb@gmail.com>
2021-10-06 16:13:24 +0200

    protocol_splitter: delete non rtps or mavlink data from buffer

--- src/drivers/protocol_splitter/protocol_splitter.cpp
+++ src/drivers/protocol_splitter/protocol_splitter.cpp
@@ -568,7 +568,7 @@ void DevCommon::cleanup()
 	size_t garbage_end = 0;
 
 	if (!mavlink_available && !rtps_available) {
-		garbage_end = math::max(_read_buffer->start_mavlink, _read_buffer->start_rtps);
+		garbage_end = _read_buffer->buf_size - 1;
 
 	} else {
 		garbage_end = math::min(_read_buffer->start_mavlink, _read_buffer->start_rtps);
