b3ac7a2e979f435094deac25e289d39bee0d42bf
Matthias Grob <maetugr@gmail.com>
2021-10-07 13:51:06 +0200

    mavlink_receiver: remove unused start_helper declaration

--- src/modules/mavlink/mavlink_receiver.h
+++ src/modules/mavlink/mavlink_receiver.h
@@ -133,7 +133,6 @@ public:
 
 	bool component_was_seen(int system_id, int component_id);
 	void print_detailed_rx_stats() const;
-	static void *start_helper(void *context);
 
 private:
 	static void *start_trampoline(void *context);
