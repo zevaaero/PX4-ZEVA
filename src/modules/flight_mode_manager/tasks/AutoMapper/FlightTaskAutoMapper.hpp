3b12f541b0ffde2ed8c19c6c6944d7942589ba07
Matthias Grob <maetugr@gmail.com>
2021-11-10 21:35:38 +0100

    FlightTaskAutoMapper: move remaining methods into FlightTaskAutoLineSmooth

--- src/modules/flight_mode_manager/tasks/AutoMapper/FlightTaskAutoMapper.hpp
+++ src/modules/flight_mode_manager/tasks/AutoMapper/FlightTaskAutoMapper.hpp
@@ -54,13 +54,6 @@ public:
 	virtual ~FlightTaskAutoMapper() = default;
 
 protected:
-	void _prepareIdleSetpoints();
-	void _prepareLandSetpoints();
-	void _prepareVelocitySetpoints();
-	void _prepareTakeoffSetpoints();
-	void _preparePositionSetpoints();
-
-	void updateParams() override; /**< See ModuleParam class */
 
 	DEFINE_PARAMETERS_CUSTOM_PARENT(FlightTaskAuto,
 					(ParamFloat<px4::params::MPC_LAND_SPEED>) _param_mpc_land_speed,
@@ -85,5 +78,5 @@ protected:
 	matrix::Vector3f _land_position;
 	float _land_heading;
 	WaypointType _type_previous{WaypointType::idle}; /**< Previous type of current target triplet. */
-	bool _highEnoughForLandingGear(); /**< Checks if gears can be lowered. */
+
 };
