81afef4737ad4b774a2a543f5171a37f918b806b
Julian Oes <julian@oes.ch>
2021-12-07 10:18:24 +0100

    mavlink: bump max remote components
    Stack does not need to be increased as the instance is allocated on the
    heap.

--- src/modules/mavlink/mavlink_receiver.h
+++ src/modules/mavlink/mavlink_receiver.h
@@ -253,7 +253,7 @@ private:
 
 	orb_advert_t _mavlink_log_pub{nullptr};
 
-	static constexpr unsigned MAX_REMOTE_COMPONENTS{8};
+	static constexpr unsigned MAX_REMOTE_COMPONENTS{12};
 	struct ComponentState {
 		uint32_t last_time_received_ms{0};
 		uint32_t received_messages{0};
