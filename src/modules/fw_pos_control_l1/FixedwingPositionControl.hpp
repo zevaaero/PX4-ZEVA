PX4/PX4-Autopilot

handle line segment termination in navigator - if following line segment (fixed-wing position control) switch waypoint when in acceptance radius OR passed the second waypoint. this handles the case of being beyond the second waypoint but not within the acceptance radius without the need to fly back to the waypoint (e.g. after a loiter up to waypoint alt) - sync navigator and fw pos ctrl waypoint acceptance altitude

src/modules/fw_pos_control_l1/FixedwingPositionControl.hpp
@@ -447,7 +447,9 @@ private:
 
 		(ParamFloat<px4::params::NAV_LOITER_RAD>) _param_nav_loiter_rad,
 
-		(ParamFloat<px4::params::FW_TKO_PITCH_MIN>) _takeoff_pitch_min
+		(ParamFloat<px4::params::FW_TKO_PITCH_MIN>) _takeoff_pitch_min,
+
+		(ParamFloat<px4::params::NAV_FW_ALT_RAD>) _param_nav_fw_alt_rad
 
 	)
 

zevaaero/PX4-ZEVA

src/modules/fw_pos_control_l1/FixedwingPositionControl.hpp
@@ -486,7 +486,9 @@ private:
 		(ParamFloat<px4::params::FW_ECO_ALT_MIN>) _param_fw_eco_alt_min,
 		(ParamFloat<px4::params::FW_ECO_AD_THRLD>) _param_fw_eco_ad_thrld,
 
-		(ParamFloat<px4::params::FW_WIND_ARSP_SC>) _param_fw_wind_arsp_sc
+		(ParamFloat<px4::params::FW_WIND_ARSP_SC>) _param_fw_wind_arsp_sc,
+
+		(ParamFloat<px4::params::NAV_FW_ALT_RAD>) _param_nav_fw_alt_rad
 	)
 
 };