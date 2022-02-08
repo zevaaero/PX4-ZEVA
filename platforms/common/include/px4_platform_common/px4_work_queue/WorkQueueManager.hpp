f2c494a325a6829d0b6d625e0f1cb3fce1399fcd
Beat Küng <beat-kueng@gmx.net>
2022-02-08 13:41:30 +0100

    WorkQueueManager: increase stack size for wq:SPI2 from 2336 to 2360
    There are rare occasions with SPI2 stack low (288 B left).
    This runs the icm42688p, not sure where it's coming from. Perf counters
    are 0.

--- platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
+++ platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -53,7 +53,7 @@ static constexpr wq_config_t ctrl_alloc{"wq:ctrl_alloc", 9500, 0}; // PX4 contro
 
 static constexpr wq_config_t SPI0{"wq:SPI0", 2336, -1};
 static constexpr wq_config_t SPI1{"wq:SPI1", 2336, -2};
-static constexpr wq_config_t SPI2{"wq:SPI2", 2336, -3};
+static constexpr wq_config_t SPI2{"wq:SPI2", 2360, -3};
 static constexpr wq_config_t SPI3{"wq:SPI3", 2336, -4};
 static constexpr wq_config_t SPI4{"wq:SPI4", 2336, -5};
 static constexpr wq_config_t SPI5{"wq:SPI5", 2336, -6};
