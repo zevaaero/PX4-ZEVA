0146458c6232873830f9204a0f2f16eba3b342eb
Matthias Grob <maetugr@gmail.com>
2022-03-07 17:57:16 +0100

    PreFlightCheck: only allow modes suitable for takeoff

--- src/modules/commander/Arming/PreFlightCheck/PreFlightCheck.hpp
+++ src/modules/commander/Arming/PreFlightCheck/PreFlightCheck.hpp
@@ -119,6 +119,7 @@ private:
 					 const bool prearm);
 
 	static bool manualControlCheck(orb_advert_t *mavlink_log_pub, const bool report_fail);
+	static bool modeCheck(orb_advert_t *mavlink_log_pub, const bool report_fail, const vehicle_status_s &status);
 	static bool airframeCheck(orb_advert_t *mavlink_log_pub, const vehicle_status_s &status);
 	static bool cpuResourceCheck(orb_advert_t *mavlink_log_pub, const bool report_fail);
 	static bool externalUpdateCheck(orb_advert_t *mavlink_log_pub, const bool report_fail);
